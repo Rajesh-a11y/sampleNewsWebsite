cfc60876ae238e0691f63ae599feb33bb376c42f
Rajesh-a11y <rajesh.sagayanathan@gmail.com>
2022-07-01 19:29:46 +0200

    modified the existing code until this point and added the header & footer content

--- sample-news-website/src/components/common/header/Header.jsx
+++ sample-news-website/src/components/common/header/Header.jsx
@@ -1,26 +1,42 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import Head from './Head'
+import React, { useState } from "react"
+import Head from "./Head"
 import "./header.css"
+import { Link } from "react-router-dom"
 
 const Header = () => {
+  const [navbar, setNavbar] = useState(false)
+
   return (
     <>
       <Head />
       <header>
-        <div className="container paddingSmall">
+        <div className='container paddingSmall'>
           <nav>
-            <ul>
-              <li><Link to='/'>Home</Link></li>
-              <li><Link to='/culture'>Culture</Link></li>
-              <li><Link to='/politics'>Politics</Link></li>
-              <li><Link to='/memes'>Memes</Link></li>
-              <li><Link to='/sports'>Sports</Link></li>
-              <li><Link to='/boxed'>Boxed</Link></li>
-              <li><Link to='/reviews'>Reviews</Link></li>
+            <ul className={navbar ? "navbar" : "flex"} onClick={() => setNavbar(false)}>
+              <li>
+                <Link to='/'>Home</Link>
+              </li>
+              <li>
+                <Link to='/culture'>Culture</Link>
+              </li>
+              <li>
+                <Link to='/politics'>Politics</Link>
+              </li>
+              <li>
+                <Link to='/memes'>Memes</Link>
+              </li>
+              <li>
+                <Link to='/sports'>Sports</Link>
+              </li>
+              <li>
+                <Link to='/boxed'>Boxed</Link>
+              </li>
+              <li>
+                <Link to='/reviews'>Reviews</Link>
+              </li>
             </ul>
-            <button className='barIco'>
-              <i className='fa fa-bars'></i>
+            <button className='barIcon' onClick={() => setNavbar(!navbar)}>
+              {navbar ? <i className='fa fa-times'></i> : <i className='fa fa-bars'></i>}
             </button>
           </nav>
         </div>
